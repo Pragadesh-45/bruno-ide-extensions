480a674396c597b3ab45cf761d52d9ef80f63825
Pragadesh-45 <temporaryg7904@gmail.com>
2024-07-31 14:09:04 +0530

    proper quotes

--- packages/vscode/src/bruno/index.js
+++ packages/vscode/src/bruno/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import ReduxStore from './providers/Store';
-import AppProvider from './providers/App';
-import ResponsePane from './components/ResponsePane';
-import 'tailwindcss/dist/tailwind.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import ReduxStore from "./providers/Store";
+import AppProvider from "./providers/App";
+import ResponsePane from "./components/ResponsePane";
+import "tailwindcss/dist/tailwind.min.css";
 
 const App = () => (
   <Provider store={ReduxStore}>
